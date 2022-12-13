07bfb98d1466a63751a48ed80bf395426a14492a
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-13 12:15:15 +0330

    minor change

--- tts.py
+++ tts.py
@@ -8,8 +8,8 @@ def synthesize_to_speaker(text):
     result = synthesizer.start_speaking_text_async(text).get()
     
     # create a stream to output (OutputStream)
-    audio_streamer = pyaudio.PyAudio()
-    stream = audio_streamer.open(format=8,channels=1,rate=16000,output=True)
+    output_streamer = pyaudio.PyAudio()
+    output_stream = output_streamer.open(format=8,channels=1,rate=16000,output=True)
     
     # create speech audio stream (InputStream) and read chunk by chunk and send it to OutputStream
     audio_data_stream = speechsdk.AudioDataStream(result)
@@ -17,7 +17,7 @@ def synthesize_to_speaker(text):
     filled_size = audio_data_stream.read_data(audio_buffer)
     while filled_size > 0:
         print(f"{filled_size} bytes received.")
-        stream.write(audio_buffer)
+        output_stream.write(audio_buffer)
         audio_buffer = bytes(16000)
         filled_size = audio_data_stream.read_data(audio_buffer)
 
